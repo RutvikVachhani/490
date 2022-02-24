81773deba605fcea2cce81c59a45fe08dacf2de8
Gerardo Baron <gerardo.baron@hotmail.com>
2022-02-24 00:39:25 -0800

    code to connect to local database

--- db_connect.js
+++ db_connect.js
@@ -1,12 +1,18 @@
 //Connect to database
-var mysql = require("mysql");
+require("dotenv").config();
+const mysql = require("mysql2");
 
-var con =  mysql.createPool({
-  host: "us-cdbr-east-05.cleardb.net",
-  user: "b14065fc30e286",
-  password: "68d7aec9",
-  database: "heroku_6ee4a0fcdd21b51",
+const con = mysql.createPool({
+  host: process.env.DB_HOST,
+  user: process.env.DB_USER,
+  password: process.env.DB_PASSWORD,
+  database: process.env.DB_NAME,
   multipleStatements: true,
 });
 
+let sql = "SELECT * FROM Student;";
+con.execute(sql, function (err, result) {
+  if (err) throw err;
+  console.log("Connected!");
+});
 module.exports = con;
